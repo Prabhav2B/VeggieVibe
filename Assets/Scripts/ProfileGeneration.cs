1648c2e9fbf7ee7d11190999464cf50986405240
Abhishek Jaiswal <55433215+abhishekjaj446@users.noreply.github.com>
2023-02-04 18:19:38 +0200

    Updated procedural generation
    Added random scale, rotation, and color

--- Assets/Scripts/ProfileGeneration.cs
+++ Assets/Scripts/ProfileGeneration.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class ProfileGeneration : MonoBehaviour
 {
     [SerializeField] SpriteRenderer stalk;
@@ -12,6 +12,16 @@ public class ProfileGeneration : MonoBehaviour
     [SerializeField] Sprite[] stalkOptions;
     [SerializeField] Sprite[] faceOptions;
     [SerializeField] Sprite[] bodyOptions;
+
+    [SerializeField] Vector2 randomBodyScaleX;
+    [SerializeField] Vector2 randomBodyScaleY;
+    [SerializeField] Vector2 randomStalkScaleX;
+    [SerializeField] Vector2 randomStalkScaleY;
+    [SerializeField] Vector2 randomRotation;
+
+    [SerializeField] Color[] randomStalkColors;   
+    [SerializeField] Color[] randomBodyColors;   
+
 
 
     void Start()
@@ -33,5 +43,16 @@ public class ProfileGeneration : MonoBehaviour
             face.sprite = faceOptions[Random.Range(0, faceOptions.Length)];
         if(bodyOptions.Length>0)
             body.sprite = bodyOptions[Random.Range(0, bodyOptions.Length)];
+
+        body.transform.localScale = new Vector2(Random.Range(randomBodyScaleX.x,randomBodyScaleX.y ),
+                                                    Random.Range(randomBodyScaleY.x, randomBodyScaleY.y));
+
+        stalk.transform.localScale = new Vector2(Random.Range(randomStalkScaleX.x, randomStalkScaleX.y),
+                                                    Random.Range(randomStalkScaleY.x, randomStalkScaleY.y));
+
+        stalk.transform.eulerAngles = new Vector3(0,0,Random.Range(randomRotation.x, randomRotation.y));
+
+        stalk.color = randomStalkColors[Random.Range(0,randomStalkColors.Length)];
+        body.color = randomBodyColors[Random.Range(0, randomBodyColors.Length)];
     }
 }
